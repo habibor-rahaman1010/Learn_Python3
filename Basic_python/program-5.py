b27c31d744878b85467577f34f310cffe2fc2956
habibor-rahaman1010 <habibor144369@gmail.com>
2023-05-27 11:23:57 +0600

    ok

--- Basic_python/program-5.py
+++ Basic_python/program-5.py
@@ -1,5 +1,32 @@
 #while loop in python program....
+
 initial = 1
 while(initial <= 10):
     print(initial)
     initial += 1
+
+
+#even number findout in python...
+i = 2
+while(i <= 10):
+    if(i % 2 == 0):
+        print(i)
+    i += 1
+
+
+#odd number findout in python...
+i = 1
+while(i <= 10):
+    if(i % 2 != 0):
+        print(i)
+    i += 1
+
+# use break and continue in python program...
+j = 1
+while(j < 10):
+    print(j)
+    if(j == 5):
+        break
+    j += 1
+
+# continue useing in pytohn program...
