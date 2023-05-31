e187cf41626011e4eaed1040172cd1f42175c86d
habibor-rahaman1010 <habibor144369@gmail.com>
2023-05-31 15:45:59 +0600

    ok done

--- OOP/bank/bank.py
+++ OOP/bank/bank.py
@@ -1,5 +1,34 @@
 # create simble bank in python use oop
 
 class Bank:
-    def __init__(self) -> None: ()
+    def __init__(self, balance) -> None:
+        self.balance = balance
+        self.min_withdraw = 100
+        self.max_withdraw = 100000
        
+    def get_balance(self):
+        return self.balance
+    
+    def diposit(self, amount):
+        if(amount > 0):
+            self.balance += amount
+            print(f'diposited: {amount}')
+            print(f'Current balance: {self.balance}')
+        else:
+            print(f'Please enter positive number!')
+
+
+    def withdraw(self, amount):
+        if(amount >= self.min_withdraw and amount <= self.max_withdraw):
+            self.balance -= amount
+            print(f'Withdraw amount: {amount}')
+            print(f'Current balance: {self.balance}')
+        else:
+            print(f'You have minimum withdraw: {self.min_withdraw} and maximum withdraw: {self.max_withdraw}')
+
+
+my_bank = Bank(12000)
+print(f'Balance: {my_bank.get_balance()}')
+my_bank.diposit(5000)
+my_bank.withdraw(3000)
+my_bank.diposit(10800)
