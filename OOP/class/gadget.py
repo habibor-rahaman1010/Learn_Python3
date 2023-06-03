9bb48a5b4ed2d53f58d9fd5afdda51058d08003b
habibor-rahaman1010 <habibor144369@gmail.com>
2023-06-03 12:58:13 +0600

    Hello programmer

--- OOP/class/gadget.py
+++ OOP/class/gadget.py
@@ -1,21 +1,14 @@
 # Inheritance implement in python using oop concept
 
-class Gedget:
-    def __init__(self, model, brand, price, color, origin) -> None:
+class Laptop:
+    def __init__(self, model, brand, type, price, color, ram, ssd) -> None:
         self.model = model
         self.brand = brand
+        self.type = type
         self.price = price
         self.color = color
-        self.origin = origin
-    def run(self):
-        print(f'My {self.model} is using right now')
-
-class Laptop(Gedget):
-    def __init__(self, model, brand, price, color, origin, type, ram, ssd) -> None:
-        self.type = type
         self.ram = ram
         self.ssd = ssd
-        super().__init__(model, brand, price, color, origin)
 
     def __repr__(self) -> str:
         return repr({
@@ -24,20 +17,25 @@ class Laptop(Gedget):
             'type': self.type,
             'price': self.price,
             'color': self.color,
-            'origin': self.origin,
             'ram': self.ram,
             'ssd': self.ssd,
         })
+    
+    def run(self):
+        print(f'My laptop {self.model} is running right now')
         
     def coding(self):
         print(f'I am right now coding by my {self.model} laptop')
 
-class Phone(Gedget):
-    def __init__(self, model, brand, price, color, origin, ram, dualSime, touchScreen) -> None:
+class Phone:
+    def __init__(self, model, brand, price, color, ram, dualSime, touchScreen) -> None:
+        self.model = model
+        self.brand = brand
+        self.price = price
+        self.color = color
         self.ram = ram
         self.dualSime = dualSime
         self.touchScreen = touchScreen
-        super().__init__(model, brand, price, color, origin)
 
     def __repr__(self) -> str:
         return repr({
@@ -45,19 +43,24 @@ class Phone(Gedget):
             'brand': self.brand,
             'price': self.price,
             'color': self.color,
-            'origin': self.origin,
             'ram': self.ram,
             'dualSime': self.dualSime,
             'touchScreen': self.touchScreen,
         })
+
+    def run(self):
+        print(f'My phone {self.model} is running right now')
         
     def phone_call(self, number, text):
         print(f'Sending SMS to {number} with text: {text}')
       
-class Camera(Gedget):
-    def __init__(self, model, brand, price, color, origin, pixel) -> None:
+class Camera:
+    def __init__(self, model, brand, price, color, pixel) -> None:
+        self.model = model
+        self.brand = brand
+        self.price = price
+        self.color = color
         self.pixel = pixel
-        super().__init__(model, brand, price, color, origin)
 
     def __repr__(self) -> str:
         return repr({
@@ -65,17 +68,19 @@ class Camera(Gedget):
             'brand': self.brand,
             'price': self.price,
             'color': self.color,
-            'origin': self.origin,
             'pixel': self.pixel,
         })
+
+    def run(self):
+        print(f'My camera {self.model} is using right now')
 
     def change_lance(self):
         print(f'Yes you can change the lance of your {self.model} camera')
 
 
-laptop = Laptop('Hp Elitbook 8470p', 'HP', 26500, 'Silver', 'Bangladesh', 'Normal User','hp 8 GB', 'Trancent 256 GB')
-phone = Phone('iphone 14', 'Apple', 186000, 'Deep Blue', 'Califonia', '6 GB', True, True)
-camara = Camera('Canon D80', 'Canon', 65500, 'Black', 'USA', '20 pixel')
+laptop = Laptop('Hp Elitbook 8470p', 'HP', 'Normal User', 26500, 'Silver', 'hp 4 GB', 'Trancent 256 GB')
+phone = Phone('iphone 14', 'Apple', 186600, 'Blue Black', '8 GB', True, True)
+camara = Camera('Canon D80', 'Canon', 65500, 'Black', '20 pixel')
 
 print(laptop)
 print(phone)
